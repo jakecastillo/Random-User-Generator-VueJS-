0bc718faf626416a4d9fd02522d6927110364bb8
Jake Castillo <jakecast@hawaii.edu>
2021-03-02 16:30:43 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+//Random user generator pulling from randomuser.me api
+//Builds in Visual Studio using live server
+
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+          }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            //console.log(results)
+
+            this.firstName = results[0].name.first,
+            this.lastName = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large,
+            console.log(this.firstName)
+        }
+    }
+})
+  
+app.mount('#app')
+  
